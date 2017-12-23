5cb14953c659545f73744653f09d2d7769b4cd1b
Jeppe Stærk <jeppe@staerk.io>
2017-12-23 05:12:32 +0100

    Refactor

--- app/src/main/java/dk/au/ase/itsmap/e17/appproject/gruppe7/udecide/utils/CONST.java
+++ app/src/main/java/dk/au/ase/itsmap/e17/appproject/gruppe7/udecide/utils/CONST.java
@@ -1,34 +1,22 @@
 package dk.au.ase.itsmap.e17.appproject.gruppe7.udecide.utils;
 
 public class CONST {
-    public static final int REQUEST_NEXT_IMAGE = 1;
-    public static final int RESULT_REMOVED = 2;
-
-    public static final String PERSON_IMAGE = "person_image";
-    public static final String PERSON_NAME = "person_name";
-    public static final String QUESTION_TEXT = "question";
-    public static final String VOTE = "vote";
-    public static final String USERNAME = "username";
-    public static final String FIRST_QUESTION_IMAGE = "first_image";
-    public static final String SECOND_QUESTION_IMAGE = "second_image";
-    public static final String UPDATE_EVENT = "update-event";
-    public static final String DB_POLLS_COLLECTION = "polls";
+    public static final String STORAGE_IMAGES_PATH = "images/";
     public static final String IMAGE_1_VOTE_KEY = "image1Votes";
     public static final String IMAGE_2_VOTE_KEY = "image2Votes";
-    public static final String DB_DATE = "date";
-    public static final String LAST_POLL_TIMESTAMP = "lastPollTimestamp";
 
-    public static final String DB_USERS_COLLECTION = "users";
-    public static final String STORAGE_IMAGES_PATH = "images/";
-    public static final String DB_SHOW_FOR_PUBLIC = "showForPublic";
+    public static final String DB_POLLS_COLLECTION = "polls";
+    public static final String DB_DATE = "date";
     public static final String DB_USER_ID = "userID";
+
+    public static final String SHARED_PREFERENCES = "SharedPreferences";
+    public static final String LAST_POLL_TIMESTAMP = "lastPollTimestamp";
 
     public static final String FACEBOOK_ID = "facebookId";
     public static final String FACEBOOK_NAME = "facebookName";
     public static final String FACEBOOK_PHOTO_URL = "facebookPhotoUrl";
     public static final String FACEBOOK_FRIENDS_IDS = "facebookFriendsIds";
 
-    public static final String SHARED_PREFERENCES = "SharedPreferences";
     public static final String NOTIFY_CHANNEL = "uDecide";
     public static final int NOTIFY_ID = 5246;
 }
