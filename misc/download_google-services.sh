482912c775e767ca4f94a53f6b611fc6bf9fdc89
Jeppe Stærk <jeppe@staerk.io>
2018-01-25 00:37:41 +0100

    CircleCI updated

--- misc/download_google-services.sh
+++ misc/download_google-services.sh
@@ -2,6 +2,7 @@
 if [[ $GOOGLE_SERVICE_FILE && ${GOOGLE_SERVICE_FILE} && $GOOGLE_SERVICE_FILE_URL && ${GOOGLE_SERVICE_FILE_URL} ]]
 then
     echo "Keystore detected - downloading..."
+    echo "File: " $GOOGLE_SERVICE_FILE " URL: " $GOOGLE_SERVICE_FILE_URL
     mkdir -p $GOOGLE_SERVICE_FILE
     curl -L -o ${GOOGLE_SERVICE_FILE} ${GOOGLE_SERVICE_FILE_URL}
 else
