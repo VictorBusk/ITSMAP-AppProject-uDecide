37f83a8c3f7f08eee2174ccfbdd36f214bbaac1a
Jeppe Stærk <jeppe@staerk.io>
2018-01-25 00:33:47 +0100

    CircleCI updated

--- misc/download_keystore.sh
+++ misc/download_keystore.sh
@@ -1,11 +1,9 @@
-# use curl to download a keystore from $ANDROID_KEY_STORE_FILE_URL, if set,
-# to the path/filename set in $KEYSTORE.
-if [[ $KEYSTORE && ${KEYSTORE} && $ANDROID_KEY_STORE_FILE_URL && ${ANDROID_KEY_STORE_FILE_URL} ]]
+#!/usr/bin/env bash
+if [[ $ANDROID_KEY_STORE_FILE && ${ANDROID_KEY_STORE_FILE} && $ANDROID_KEY_STORE_FILE_URL && ${ANDROID_KEY_STORE_FILE_URL} ]]
 then
-    echo "Keystore detected - downloading..."
-    # we're using curl instead of wget because it will not
-    # expose the sensitive uri in the build logs:
-    curl -L -o ${KEYSTORE} ${ANDROID_KEY_STORE_FILE_URL}
+    echo "Google Service detected - downloading..."
+    mkdir -p $ANDROID_KEY_STORE_FILE
+    curl -L -o ${ANDROID_KEY_STORE_FILE} ${ANDROID_KEY_STORE_FILE_URL}
 else
-    echo "Keystore uri not set.  .APK artifact will not be signed."
+    echo "Google Service uri not set."
 fi
