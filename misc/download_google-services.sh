b78d55579a3e3d6c3c9df5e0b0a4d7250db8f3f6
Jeppe Stærk <jeppe@staerk.io>
2018-01-25 00:52:01 +0100

    CircleCI updated

--- misc/download_google-services.sh
+++ misc/download_google-services.sh
@@ -2,9 +2,7 @@
 if [[ $GOOGLE_SERVICE_FILE && ${GOOGLE_SERVICE_FILE} && $GOOGLE_SERVICE_FILE_URL && ${GOOGLE_SERVICE_FILE_URL} ]]
 then
     echo "Keystore detected - downloading..."
-    mkdir -p ${GOOGLE_SERVICE_FILE}
     curl -L -o ${GOOGLE_SERVICE_FILE} ${GOOGLE_SERVICE_FILE_URL}
-    ls ~/repo/app
 else
     echo "Keystore uri not set."
 fi
--- misc/download_keystore.sh
+++ misc/download_keystore.sh
@@ -2,9 +2,7 @@
 if [[ $ANDROID_KEY_STORE_FILE && ${ANDROID_KEY_STORE_FILE} && $ANDROID_KEY_STORE_FILE_URL && ${ANDROID_KEY_STORE_FILE_URL} ]]
 then
     echo "Google Service detected - downloading..."
-    mkdir -p ${ANDROID_KEY_STORE_FILE}
     curl -L -o ${ANDROID_KEY_STORE_FILE} ${ANDROID_KEY_STORE_FILE_URL}
-    ls ~/repo
 else
     echo "Google Service uri not set."
 fi
