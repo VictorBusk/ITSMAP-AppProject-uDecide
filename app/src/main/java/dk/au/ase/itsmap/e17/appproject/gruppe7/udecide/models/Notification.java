1645e008411eed32c2861b291f9c004e0d6df502
Jeppe Stærk <jeppe@staerk.io>
2018-01-25 13:19:01 +0100

    Notification - Model added BackgroundService - Firebase eventListener refectored - Test joke deleted - SendNotification made async and updated for better backwards support

---
+++ app/src/main/java/dk/au/ase/itsmap/e17/appproject/gruppe7/udecide/models/Notification.java
@@ -0,0 +1,41 @@
+package dk.au.ase.itsmap.e17.appproject.gruppe7.udecide.models;
+
+public class Notification {
+
+    private int id;
+    private String title;
+    private String text;
+
+    public Notification() {
+    }
+
+    public Notification(int id, String title, String text) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
